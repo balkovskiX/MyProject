4ba4f39b75f9f24b8e1b671db77d610ca6bb2b58
balkovskiX <balkovskionn@gmail.com>
2023-09-07 19:04:20 +0400

    delete

--- Hello World!.java
+++
@@ -1,7 +0,0 @@
-package com.balkovskion.cs.com;
-
-public class balkovskion {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- Lesson17Runner.java
+++
@@ -1,7 +0,0 @@
-package lesson17;
-
-public class Lesson17Runner {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
